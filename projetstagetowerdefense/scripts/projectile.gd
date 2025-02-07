8dae2404a17a09c9af14a8ee231ed9a78505078f
Ewhae <ewen.hamono@gmail.com>
2025-02-07 16:32:08 -0500

    choix cible projectile

--- projetstagetowerdefense/scripts/projectile.gd
+++ projetstagetowerdefense/scripts/projectile.gd
@@ -3,20 +3,19 @@ extends Area2D
 @export var speed: float = 300  
 @export var damage: int = 25  
 
-var target_position: Vector2 = Vector2.ZERO
+var target: Node2D = null
 var direction: Vector2 = Vector2.ZERO
 
 func _ready():
 	set_process(true)
 #Ca marche pas dutout j'arrive pas à afficher ce qui me sert de projectile mais les mecs prennent des dégats donc OKLM 
 func _process(delta):
-	if target_position != Vector2.ZERO:
+	if target.position != Vector2.ZERO:
+		direction = (target.position - position).normalized()
 		position += direction * speed * delta
-		if position.distance_to(target_position) < 10:
+		if position.distance_to(target.position) < 10:
 			on_hit_target()
 
 func on_hit_target():
-	var enemy = get_parent().get_node_or_null("Enemy") 
-	if enemy and enemy.is_in_group("enemies"):
-		enemy.take_damage(damage)
+	target.take_damage(damage)
 	queue_free() 
--- projetstagetowerdefense/scripts/tower.gd
+++ projetstagetowerdefense/scripts/tower.gd
@@ -4,8 +4,8 @@ extends Node2D
 @export var projectile_scene: PackedScene
 @export var shoot_interval: float = 1.0  
 
-var enemie_cible: Node2D = null
-var liste_ennemi = []
+var enemy_target: Node2D = null
+var list_enemies = []
 
 @onready var barre_vie_tour = $ProgressBar  
 @onready var timer = $Timer  
@@ -18,25 +18,31 @@ func _ready():
 	timer.start()  
 	
 	detection_area.body_entered.connect(_on_enemy_entered)
-	detection_area.body_exited.connect(_on_enemy_exited) 
+	detection_area.body_exited.connect(_on_enemy_exited)
 
 func _on_enemy_entered(enemy):
-	if enemy.is_in_group("enemies"): 
-		enemie_cible = enemy  
-		liste_ennemi.append(enemy)
-		#print(liste_ennemi) POUR MES TESTS MAIS CA FONCTIONNE PAS DUTOUT JAI ESSAYE UNE LOGIQUE MAUVAISE JE PENSE
+	list_enemies.append(enemy)
+	if enemy_target == null:  
+		change_target()
 		
 
 func _on_enemy_exited(enemy):
-	if enemy == enemie_cible:  
-		enemie_cible = null
+	list_enemies.erase(enemy)
+	change_target()
 
+func change_target():
+	if list_enemies.size() > 0 :
+		enemy_target = list_enemies[0]
+	else :
+		enemy_target = null
+	print(enemy_target)
+	
 func shoot_projectile():
-	if enemie_cible and projectile_scene:
+	print(enemy_target)
+	if enemy_target and projectile_scene:
 		print("La tour tire sur l'ennemi")
 		var projectile = projectile_scene.instantiate()
 		get_parent().add_child(projectile)
 		projectile.position = position
-		projectile.target_position = enemie_cible.position
-		projectile.direction = (enemie_cible.position - position).normalized()
+		projectile.target = enemy_target
 		projectile.speed = 300
