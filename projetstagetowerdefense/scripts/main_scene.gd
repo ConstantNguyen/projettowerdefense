65c4502186b739ea60f62a892563237a1da8acee
Constant NGUYEN <constant.nguyen@iut-rodez.fr>
2025-04-16 15:53:48 -0400

    Ajout timer

--- projetstagetowerdefense/scripts/main_scene.gd
+++ projetstagetowerdefense/scripts/main_scene.gd
@@ -1,8 +1,26 @@
 extends Node3D
 
+@onready var timer_label = $CanvasLayer/timer_label
+@onready var game_timer = $game_timer
+@onready var bouton_pause = $button_pause
+
+var seconds_passed = 0
 
 func _ready() :
-	pass # Replace with function body.
+	game_timer.timeout.connect(_on_game_timer_timeout)
+	game_timer.start()
+	update_timer_display()
+
+func _on_game_timer_timeout():
+	seconds_passed += 1
+	update_timer_display()
+
+
+func update_timer_display():
+	var minutes = seconds_passed / 60
+	var seconds = seconds_passed % 60
+	var formatted_time = "%02d:%02d" % [minutes, seconds]
+	timer_label.text = formatted_time
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,5 +34,3 @@ func game_over():
 		get_tree().root.add_child(menu_scene)
 		queue_free()
 		
-
-			
