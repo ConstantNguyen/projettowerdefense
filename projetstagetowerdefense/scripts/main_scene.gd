1b86943145377ffeda4bb61bc491eb236abe0394
Rabab BOULKRIAT <Rababboulkriat@outlook.fr>
2025-04-16 17:09:17 -0400

    Bouton de pause

--- projetstagetowerdefense/scripts/main_scene.gd
+++ projetstagetowerdefense/scripts/main_scene.gd
@@ -5,32 +5,35 @@ extends Node3D
 @onready var bouton_pause = $CanvasLayer/button_pause
 
 var seconds_passed = 0
+var is_paused = false
 
-func _ready() :
+func _ready():
 	game_timer.timeout.connect(_on_game_timer_timeout)
+	bouton_pause.pressed.connect(_on_pause_button_pressed)
 	game_timer.start()
 	update_timer_display()
+	
+
+
+func _on_pause_button_pressed():
+	if is_paused:
+		is_paused = false
+		get_tree().paused = false
+		bouton_pause.text = "Pause"
+		print("Jeu repris !")
+	else:
+		is_paused = true
+		get_tree().paused = true
+		bouton_pause.text = "Reprendre"  
+		print("Jeu en pause !")  
 
 func _on_game_timer_timeout():
-	seconds_passed += 1
-	update_timer_display()
-
+	if not is_paused:
+		seconds_passed += 1
+		update_timer_display()
 
 func update_timer_display():
 	var minutes = seconds_passed / 60
 	var seconds = seconds_passed % 60
 	var formatted_time = "%02d:%02d" % [minutes, seconds]
 	timer_label.text = formatted_time
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-func game_over():
-	if get_tree().get_nodes_in_group("towers").size() == 0 : 
-		var menu_scene = preload("res://scenes/menu1.tscn").instantiate()
-		menu_scene.get_child(0).is_game_over = true
-		get_tree().root.add_child(menu_scene)
-		queue_free()
-		
